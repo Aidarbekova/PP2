d26049fb2be7f583b3449b04e3ee4f098685689a
Aidarbekova <bratz_05@mail.ru>
2019-02-13 12:10:57 +0600

    abc

--- ConsoleApp7/ConsoleApp7/Program.cs
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -8,24 +8,44 @@ namespace ConsoleApp7
 {
     class Student
     {
-        static void Main(string[] args)
-        {
-            public name;
-            public surname ;
-            double year;
-            public void Init(string name,string surname)
-            {
-                this.name = name;
-                this surname = surname;
-            }
+        private int id = 0;
+        public string name = "";
+        public int yearOfStudy = 0;
 
+        public Student() { }
+
+        public Student(string n, int id)
+
+        {
+            name = n;
+            this.id = id;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+        public void incrementYearOfStudy()
+        {
+            this.yearOfStudy++;
         }
     }
     class Program
     {
+
         static void Main(string[] args)
         {
-            
+            Student s = new Student("a", 1);
+            Console.WriteLine(s.getId());
+            s.getId();
+            s.incrementYearOfStudy();
+            Console.WriteLine(s.yearOfStudy);
+
+            Console.ReadKey();
         }
     }
 }
+
+
+
+
+
